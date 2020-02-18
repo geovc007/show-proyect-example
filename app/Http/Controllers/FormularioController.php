bfa85b36e3a8b11ed99d911ee7ede1157ed6e026
Geovanny Cordova <geo_cor@hotmail.com>
2020-02-18 18:23:21 -0500

    api repuestos falta ciudad

--- app/Http/Controllers/FormularioController.php
+++ app/Http/Controllers/FormularioController.php
@@ -26,7 +26,7 @@ class FormularioController extends Controller
                     'apellido' => $request->apellido,
                     'celular' => $request->celular,
                     'email' => $request->email,
-                    'modelo' => $request->modelo,
+                    'modelo' => $request->vehiculo_modelo_nombre,
                     'comentarios' => $request->comentarios,
                     'fecha_creacion' => date("Y-m-d H:i:s") 
                 ]
@@ -35,19 +35,20 @@ class FormularioController extends Controller
             if($pesados == true)
             {
                 $data = [
-                    'subject'   => 'Informacion Pesados - '.strtoupper($request->vehiculo_modelo_nombre),
+                    'subject'   => 'Informacion de Contacto - '.strtoupper($request->vehiculo_modelo_nombre),
                     'from'      => 'hyundai@curbe.com.ec',
                     'from_name' => 'Hyundai Ecuador',
+                    'contacto' => 'Juan Crespo',
                     'nombre'=> $request->nombre,
                     'apellido' => $request->apellido,
                     'celular' => $request->celular,
                     'email' => $request->email,
-                    'modelo' => $request->modelo,
+                    'modelo' => $request->vehiculo_modelo_nombre,
                     'comentarios' => $request->comentarios,
                     'fecha_creacion' => date("Y-m-d H:i:s")
                 ];
                 // Obtener Remitentes
-                Mail::to($request->email, $request->nombre)->send(new PesadosMail($data));
+                Mail::to($request->email, "Juan Crespo")->send(new PesadosMail($data));
                 $json = [ 'status'=> 'Ok', 'respuesta'=> 'Formulario Ingresado Correctamente'];
                 return $json;
             }
@@ -60,6 +61,62 @@ class FormularioController extends Controller
         } catch (Throwable $th) {
             //throw $th;
             $json = [ 'status'=> 'Error', 'respuesta'=> 'Error al Ingresar Formulario Pesados, catch'];
+            return $json;
+        }
+    }
+
+    public function repuestos(Request $request)
+    {
+        //
+        try {
+            $repuestos = DB::table('formulario_repuestos')->insert(
+                [
+                    'codigo_secundario' => $request->id,
+                    'nombre'=> $request->nombre,
+                    'apellido' => $request->apellido,
+                    'celular' => $request->celular,
+                    'email' => $request->email,
+                    'ciudad' => $request->ciudad,
+                    'modelo' => $request->vehiculo_modelo_nombre,
+                    'anio' => $request->vehiculo_anio,
+                    'chasis' => $request->vehiculo_chasis,
+                    'comentarios' => $request->comentarios,
+                    'fecha_creacion' => date("Y-m-d H:i:s") 
+                ]
+            );
+            // Verificacion
+            if($repuestos == true)
+            {
+                $data = [
+                    'subject'   => 'Informacion de Contacto - '.strtoupper($request->vehiculo_modelo_nombre),
+                    'from'      => 'hyundai@curbe.com.ec',
+                    'from_name' => 'Hyundai Ecuador',
+                    'contacto' => 'Juan Crespo',
+                    'nombre'=> $request->nombre,
+                    'apellido' => $request->apellido,
+                    'celular' => $request->celular,
+                    'email' => $request->email,
+                    'ciudad' => $request->ciudad,
+                    'modelo' => $request->vehiculo_modelo_nombre,
+                    'anio' => $request->vehiculo_anio,
+                    'chasis' => $request->vehiculo_chasis,
+                    'comentarios' => $request->comentarios,
+                    'fecha_creacion' => date("Y-m-d H:i:s") 
+                ];
+                // Obtener Remitentes
+                Mail::to($request->email, "Juan Crespo")->send(new PesadosMail($data));
+                $json = [ 'status'=> 'Ok', 'respuesta'=> 'Formulario Repuestos Ingresado Correctamente'];
+                return $json;
+            }
+            else
+            {
+                $json = [ 'status'=> 'Error', 'respuesta'=> 'Error al Ingresar Formulario Repuestos'];
+                return $json;
+            }
+
+        } catch (Throwable $th) {
+            //throw $th;
+            $json = [ 'status'=> 'Error', 'respuesta'=> 'Error al Ingresar Formulario Repuestos, catch'];
             return $json;
         }
     }
--- resources/views/emails4/html.blade.php
+++ resources/views/emails4/html.blade.php
@@ -1,9 +1,9 @@
 <div style='width:100%; background-color:rgb(246,243,241); padding: 50px 0'>
-    <div style='width:78%; background-color:rgb(255,255,255); margin-left: auto; margin-right: auto'>
+    <div style='width:78%; padding:15px; background-color:rgb(255,255,255); margin-left: auto; margin-right: auto'>
         <div style='text-align:left;'>
-            <img src="{{ asset('public/images/header/pesados.png') }}" alt="Hyundai" style='width:100%;'/>
+            <img src="{{ asset('public/images/header/header-pesados.png') }}" alt="Hyundai" style='width:100%;'/>
         </div>
-        
+        <div style='font-size:1.3em;  padding: 0 15px'>Estimado(a) <strong>{{$data['contacto']}}</strong>, la información del cliente a continuación:</div><br>
         <table rules='all' style='border-color:#666;' border='1' cellpadding='10' width='100%'>
             <tr>
                 <td align='center' bgcolor='#002c5f' colspan='2'>
@@ -15,11 +15,13 @@
                 </td>
             </tr>
             <tr><td width='150px'><strong>Nombre:</strong> </td><td>{{$data['nombre']}}</td></tr>
+            <tr><td width='150px'><strong>Apellido:</strong> </td><td>{{$data['apellido']}}</td></tr>
             <tr><td width='150px'><strong>Celular:</strong> </td><td>{{$data['celular']}}</td></tr>
             <tr><td width='150px'><strong>Email:</strong> </td><td>{{$data['email']}}</td></tr>
-            
-            
-        </table><br>
+            <tr><td width='150px'><strong>Modelo:</strong> </td><td>{{$data['modelo']}}</td></tr>
+            <tr><td width='150px'><strong>Comentarios:</strong> </td><td>{{$data['comentarios']}}</td></tr>
+            <tr><td width='150px'><strong>Fecha:</strong> </td><td>{{$data['fecha_creacion']}}</td></tr>
+        </table>
         
     </div>
 </div>
